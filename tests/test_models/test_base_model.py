cdad4053b8e32f72ab8de9ec0cfd8ca22a771df3
leudemon <avit.mugisha@gmail.com>
2023-05-24 15:00:01 +0200

    updates

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -18,6 +18,25 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(base_model.created_at, datetime)
         self.assertIsInstance(base_model.updated_at, datetime)
         self.assertEqual(base_model.created_at, base_model.updated_at)
+    
+    def test_instantiation_with_kwargs(self):
+        # 1. We create a new instance of the BaseModel class.
+        # 2. We set the id attribute to “345”.
+        # 3. We set the created_at attribute to the current time.
+        # 4. We set the updated_at attribute to the current time.
+        # 5. We check that the id attribute is set to “007”.
+        # 6. We check that the created_at attribute is set to the current time.
+        # 7. We check that the updated_at attribute is set to the current time.
+        dt = datetime.today()
+        dt_iso = dt.isoformat()
+        bm = BaseModel(id="007", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "007")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+    def test_instantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
 
     def test_base_model_save(self):
         bm = BaseModel()
