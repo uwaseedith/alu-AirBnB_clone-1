0c88c147b57e996998f84203ee086dbdc020e86c
leudemon <avit.mugisha@gmail.com>
2023-05-25 02:07:51 +0200

    updates

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -3,6 +3,7 @@ import unittest
 from models.base_model import BaseModel
 from datetime import datetime
 from time import sleep
+import os
 
 
 class TestBaseModel(unittest.TestCase):
@@ -72,6 +73,57 @@ class TestBaseModel(unittest.TestCase):
         expected_str = f"[BaseModel] ({base_model.id}) {base_model.__dict__}"
         self.assertEqual(str(base_model), expected_str)
 
+class TestBaseModel_save(unittest.TestCase):
+    """
+        Unittests to test save method of the 'BaseModel' class.
+    """
 
+    @classmethod
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+    def test_one_save(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        self.assertLess(first_updated_at, bm.updated_at)
+
+    def test_two_saves(self):
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        second_updated_at = bm.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        bm.save()
+        self.assertLess(second_updated_at, bm.updated_at)
+
+    def test_save_with_arg(self):
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.save(None)
+
+    def test_save_updates_file(self):
+        bm = BaseModel()
+        bm.save()
+        bmid = "BaseModel." + bm.id
+        with open("file.json", "r") as f:
+            self.assertIn(bmid, f.read())
 if __name__ == '__main__':
     unittest.main()
